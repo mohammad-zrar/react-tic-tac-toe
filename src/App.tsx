f8600c8f41daa8f34a5c1d64a661112ff34c1d84
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-08-24 12:40:43 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div>
+      <header>
+        <h1 className="text-4xl text-center mt-8">Tic Tac Toe Game</h1>
+      </header>
+      <main></main>
+    </div>
+  );
+}
